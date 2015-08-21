6e5d89421828957cae57408602bdf24fdf024a91
dacur <dacur7@gmail.com>
2015-08-21 13:02:02 -0400

    Allows player to go first; allows player to take turn; new tests.

--- tictactoe/player.rb
+++ tictactoe/player.rb
@@ -1,8 +1,35 @@
 class Player
 	# attr_accessor :order
 
-	def order
+	def set_order
 		p "Would you like to go first (y/n)?"
-		@order = gets.chomp
+		@order = gets.chomp.downcase
+		first_turn
+	end
+
+	def order
+		@order
+	end
+
+	def first_turn
+		if order == "y"
+			player_turn
+		elsif order == "n"
+			robot_turn
+		else
+			set_order
+		end
+	end
+
+	def player_turn
+		p "Please enter a number between 1 and 9."
+		choice = gets.chomp.to_i
+	end
+
+	def robot_turn
+		"here too"
 	end
 end
+
+
+
