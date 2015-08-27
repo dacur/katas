cac0efcdf4de1d4d46a0044709155fd67f146440
dacur <dacur7@gmail.com>
2015-08-27 09:16:02 -0400

    Adds Number class and first test.

--- roman/numbers_spec.rb
+++ roman/numbers_spec.rb
@@ -1,1 +1,11 @@
 require './numbers.rb'
+
+RSpec.describe Numbers do
+	subject(:number) {Numbers.new}
+	describe "#romanize" do
+		it "take an integer and returns a roman numeral" do
+			number.romanize(1)
+			expect(number.romanize).to eq("I")
+		end
+	end
+end
