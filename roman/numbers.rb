f6fc94aeb03c87554291b13e95438e2b32059612
dacur <dacur7@gmail.com>
2015-08-27 09:59:30 -0400

    Romanize and test are working.

--- roman/numbers.rb
+++ roman/numbers.rb
@@ -2,7 +2,14 @@ class Numbers
 	attr_accessor :romanize
 
 	def romanize(number)
-		return "I"
+		romans = {1000 => "M", 1 => "I"}
+		roman_numeral = ""
+		romans.each do |key, value|
+			number_of_rn = number/key
+			number_of_rn.times { roman_numeral += value }
+			number = number % key
+		end
+		roman_numeral
 	end
 
 end
--- roman/numbers_spec.rb
+++ roman/numbers_spec.rb
@@ -6,5 +6,8 @@ RSpec.describe Numbers do
 		it "converts 1 to I" do
 			expect(number.romanize(1)).to eq("I")
 		end
+		it "converts 2 to II" do
+			expect(number.romanize(2)).to eq("II")
+		end
 	end
 end
