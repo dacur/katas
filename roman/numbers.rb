ef00ff013cb3c8472448792f8cffb045bb7271fb
dacur <dacur7@gmail.com>
2015-08-27 09:32:28 -0400

    Cleaned up first test.

--- roman/numbers.rb
+++ roman/numbers.rb
@@ -1,3 +1,8 @@
 class Numbers
+	attr_accessor :romanize
+
+	def romanize(number)
+		return "I"
+	end
 
 end
--- roman/numbers_spec.rb
+++ roman/numbers_spec.rb
@@ -3,9 +3,8 @@ require './numbers.rb'
 RSpec.describe Numbers do
 	subject(:number) {Numbers.new}
 	describe "#romanize" do
-		it "take an integer and returns a roman numeral" do
-			number.romanize(1)
-			expect(number.romanize).to eq("I")
+		it "converts 1 to I" do
+			expect(number.romanize(1)).to eq("I")
 		end
 	end
 end
