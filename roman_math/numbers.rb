c58a5dae8a259179576ef430ffaa71ee4be7b4c9
dacur <dacur7@gmail.com>
2015-09-02 15:51:14 -0400

    Splits equation into values and operators. Converts RN to integers.

--- roman_math/numbers.rb
+++ roman_math/numbers.rb
@@ -1,3 +1,27 @@
 class Numbers
+	attr_accessor :roman_math, :equation, :values, :operators
+	attr_reader :unromanize
 
+	def romanize(number)
+		romans = {1000 => "M", 900 => "CM", 500 => "D", 400 => "CD", 100 => "C", 50=> "L", 10 => "X", 9 => "IX", 5=> "V", 4 => "IV", 1 => "I"}
+		roman_numeral = ""
+		romans.each do |key, value|
+			number_of_rn = number/key
+			number_of_rn.times { roman_numeral += value }
+			number = number % key
+		end
+		roman_numeral
+	end
+
+	def roman_math(equation)
+		@equation = equation.split(" ")
+		@values = @equation.select.each_with_index { |_, i| i.even? }
+		@operators = @equation.select.each_with_index { |_, i| i.odd? }
+	end
+
+	def unromanize(roman_numeral)
+		rn = {"I" => 1, "II" => 2, "III" => 3, "IV" => 4, "V" => 5, "VI" => 6, 
+			"VII" => 7, "VIII" => 8, "IX" => 9, "X" => 10}
+		rn[roman_numeral]
+	end
 end
--- roman_math/numbers_spec.rb
+++ roman_math/numbers_spec.rb
@@ -1,1 +1,71 @@
 require './numbers.rb'
+
+RSpec.describe Numbers do
+	subject(:number) {Numbers.new}
+	describe "#roman_math" do
+		it "takes in an equation as a string and converts it to an array" do
+			# expect(number).to receive(:unromanize)
+			number.roman_math("X + X")
+			expect(number.equation).to eq(["X", "+", "X"])
+		end
+		it "selects the numbers out of the equation" do
+			# expect(number).to receive(:unromanize)
+			number.roman_math("X + X")
+			expect(number.values).to eq(["X", "X"])
+		end
+		it "selects the operators out of the equation" do
+			# expect(number).to receive(:unromanize)
+			number.roman_math("X + X - X")
+			expect(number.operators).to eq(["+", "-"])
+		end
+	end
+
+	describe "#unromanize" do
+		it "takes a Roman Numeral and returns the equivalent integer" do
+			expect(number.unromanize("I")).to equal(1) 
+		end
+	end
+
+	# subject(:number) {Numbers.new}
+	# describe "#romanize" do
+	# 	it "converts 1 to I" do
+	# 		expect(number.romanize(1)).to eq("I")
+	# 	end
+	# 	it "converts 2 to II" do
+	# 		expect(number.romanize(2)).to eq("II")
+	# 	end
+	# 	it "converts 4 to IV" do
+	# 		expect(number.romanize(4)).to eq("IV")
+	# 	end
+	# 	it "converts 9 to IX" do
+	# 		expect(number.romanize(9)).to eq("IX")
+	# 	end
+	# 	it "converts 18 to XVIII" do
+	# 		expect(number.romanize(18)).to eq("XVIII")
+	# 	end
+	# 	it "converts 37 to XXXVII" do
+	# 		expect(number.romanize(37)).to eq("XXXVII")
+	# 	end
+	# 	it "converts 75 to LXXV" do
+	# 		expect(number.romanize(75)).to eq("LXXV")
+	# 	end
+	# 	it "converts 225 to CCXXV" do
+	# 		expect(number.romanize(225)).to eq("CCXXV")
+	# 	end
+	# 	it "converts 450 to CDL" do
+	# 		expect(number.romanize(450)).to eq("CDL")
+	# 	end
+	# 	it "converts 500 to D" do
+	# 		expect(number.romanize(500)).to eq("D")
+	# 	end
+	# 	it "converts 700 to DCC" do 
+	# 		expect(number.romanize(700)).to eq("DCC")
+	# 	end
+	# 	it "converts 900 to CM" do
+	# 		expect(number.romanize(900)).to eq("CM")
+	# 	end
+	# 	it "converts 1000 to M" do 
+	# 		expect(number.romanize(1000)).to eq("M")
+	# 	end
+
+end
