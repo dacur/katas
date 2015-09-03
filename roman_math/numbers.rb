f7a120a4b20ad24fab1406e5de985c4a1116cfa9
dacur <dacur7@gmail.com>
2015-09-03 13:22:52 -0400

    Removed unused code.

--- roman_math/numbers.rb
+++ roman_math/numbers.rb
@@ -2,17 +2,6 @@ class Numbers
 	attr_accessor :roman_math, :equation, :values, :operators, :unromanized_values, :returned_value
 	attr_reader :unromanize
 
-	def romanize(number)
-		romans = {1000 => "M", 900 => "CM", 500 => "D", 400 => "CD", 100 => "C", 50=> "L", 10 => "X", 9 => "IX", 5=> "V", 4 => "IV", 1 => "I"}
-		roman_numeral = ""
-		romans.each do |key, value|
-			number_of_rn = number/key
-			number_of_rn.times { roman_numeral += value }
-			number = number % key
-		end
-		roman_numeral
-	end
-
 	def roman_math(equation)
 		@equation = equation.split(" ")
 		@values = @equation.select.each_with_index { |_, i| i.even? }
