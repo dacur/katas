2e9945c9b5d89007c6cf093605fc6800bd74bf2f
dacur <dacur7@gmail.com>
2015-09-09 11:03:55 -0400

    Refactors GRose, including removing nested ifs.

--- gilded_rose/gilded_rose.rb
+++ gilded_rose/gilded_rose.rb
@@ -1,59 +1,61 @@
 def update_quality(items)
   items.each do |item|
-    if item.name != 'Conjured Mana Cake'
-      if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
-        if item.quality > 0
-          if item.name != 'Sulfuras, Hand of Ragnaros'
-            item.quality -= 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality += 1
-          if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality += 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality += 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != 'Sulfuras, Hand of Ragnaros'
-        item.sell_in -= 1
-      end
-      if item.sell_in < 0 
-        if item.name != "Aged Brie"
-          if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-            if item.quality > 0 
-              if item.name != 'Sulfuras, Hand of Ragnaros'
-                item.quality -= 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality += 1
-          end
-        end
-      end
+    case item.name
+    when "Sulfuras, Hand of Ragnaros"
+      next
+    when "Conjured Mana Cake"
+      handle_conjured(item)
+    when "Aged Brie"
+      handle_brie(item)
+    when "Backstage passes to a TAFKAL80ETC concert"
+      handle_backstage(item)
     else
-      item.sell_in -= 1
-      if item.quality != 0
-        if item.sell_in > 0
-          item.quality -= 2
-        else
-          item.quality -= 4
-        end
-      end
+      handle_default(item)
     end
+    if item.name != "Sulfuras, Hand of Ragnaros"
+      item.sell_in -= 1
+    end
+    handle_limits(item)
+  end
+end    
+
+def handle_limits(item)
+  if item.quality > 50 
+    item.quality = 50
+  elsif item.quality < 0
+    item.quality = 0
+  end
+end
+
+def handle_conjured(item)
+  2.times { handle_default(item) }
+end
+
+def handle_brie(item)
+  if item.sell_in > 0
+    item.quality += 1
+  else
+    item.quality += 2
+  end
+end
+
+def handle_backstage(item)
+  if item.sell_in >= 11
+    item.quality += 1
+  elsif item.sell_in < 11 && item.sell_in > 5
+    item.quality += 2
+  elsif item.sell_in < 6 && item.sell_in > 0
+    item.quality += 3
+  else
+    item.quality = 0
+  end
+end
+
+def handle_default(item)
+  if item.sell_in > 0
+    item.quality -= 1
+  else
+    item.quality -= 2
   end
 end
 
